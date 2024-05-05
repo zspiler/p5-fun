2e1e20c67c2121621f3506a992c09cccbbf69efb
Zan Spiler <zan.spiler@celtra.com>
2024-05-05 14:49:57 +0200

    slider

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 // import { randomRgb } from './util.js'
 import { generatePalette, hexToRgb, randomHex } from './colors.js'
 
+let slider1;
+
 let pallete = []
 
 function setup() {
   createCanvas(1000, 1000);
 
+  slider1 = createSlider(0, 1000, 10);
+  slider1.position(20, 900);
 
   pallete = generatePalette(randomHex())
 }
@@ -16,6 +20,9 @@ let speed = 0.004
 
 function draw() {
   background(0);
+  stroke(255)
+  fill(255)
+  text('SIZE', slider1.x * 2 + slider1.width, slider1.y + slider1.height - 2);
 
   for (let i = 0; i < pallete.length; i++) {
     const { r, g, b } = hexToRgb(pallete[i])
@@ -25,8 +32,8 @@ function draw() {
     rect(500 + i * 100, 100, 100, 100)
   }
 
-  for (let y = 50; y < 800; y += 1) {
-    step(50, y)
+  for (let y = 0; y < 1000; y += 0.1) {
+    step(slider1.value(), y)
   }
 
   time += speed
@@ -37,11 +44,11 @@ function step(x = 50, y = 50) {
   const { r, g, b } = hexToRgb(pallete[Math.floor(y) % pallete.length])
 
   fill(r, g, b)
-  stroke(255, 0, 0)
+  stroke(r, g, b)
 
   rect(sin(time + y) * x + y, y, 1, 1); // dots
 
-  rect(sin(time + y) * x + y, y, 10, 100); // skyscrapers
+  // rect(sin(time + y) * x + y, y, 10, 100); // skyscrapers
 }
 
 
